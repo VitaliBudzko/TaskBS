a29115d5987a92a9a4785af54b7d9ee5685fd21a
Vitali Budzko <labanka1983@gmail.com>
2018-07-12 21:35:05 +0300

    The image views, the text fields and the buttons have been connected with the code.

--- TaskBalinaSoft/ViewController.swift
+++ TaskBalinaSoft/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var firstBoundView: UIView!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var secondImageView: UIImageView!
+    @IBOutlet weak var secondBoundView: UIView!
+    @IBOutlet weak var forRegistrationButton: UIButton!
+    @IBAction func registrationButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        forRegistrationButton.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
